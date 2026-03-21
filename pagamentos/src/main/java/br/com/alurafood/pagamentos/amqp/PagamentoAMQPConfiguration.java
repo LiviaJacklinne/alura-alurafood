c6fc1887f6e9dd3cd0c63dbcb76a9e962c0f71ae
Lívia Jacklinne <livia.jacklinne@gec.inatel.br>
2026-03-20 21:11:23 -0300

    feat: adicionando configurções para a fila, e fazendo uma fanout exchenge

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/amqp/PagamentoAMQPConfiguration.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/amqp/PagamentoAMQPConfiguration.java
@@ -1,9 +1,12 @@
 package br.com.alurafood.pagamentos.amqp;
 
 
+import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.context.ApplicationListener;
@@ -13,12 +16,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class PagamentoAMQPConfiguration {
     
-    @Bean
-    public Queue criaFila() {
-        // criando uma fila chamada pagamento.concluido, que não é durável
-        return new Queue("pagamento.concluido", false);
+    // Não vamos precisar criar a fila mais, será tudo gerenciado pela exchange
+    // @Bean
+    // public Queue criaFila() {
+    //     // criando uma fila chamada pagamento.concluido, que não é durável
+    //     return new Queue("pagamento.concluido", false);
 
-        // return QueueBuilder.nonDurable("pagamento.concluido").build();
+    //     // return QueueBuilder.nonDurable("pagamento.concluido").build();
+    // }
+
+    @Bean
+    public FanoutExchange fanoutExchange() {
+        return new FanoutExchange("pagamentos.ex");
     }
 
     @Bean
@@ -32,4 +41,20 @@ public class PagamentoAMQPConfiguration {
         return event -> rabbitAdmin.initialize();
     }
 
+    // Formatando o body da requisição para fila
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, 
+        Jackson2JsonMessageConverter messageConverter) {
+
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+
+        return rabbitTemplate;
+    }
+    
 }
---
+++ pedidos/src/main/java/br/com/alurafood/pedidos/amqp/PedidoListener.java
@@ -0,0 +1,26 @@
+package br.com.alurafood.pedidos.amqp;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import br.com.alurafood.pedidos.dto.PagamentoDto;
+
+@Component
+public class PedidoListener {
+    
+    @RabbitListener(queues = "pagamentos.detalhes-pedido")
+    public void recebeMensagem(@Payload PagamentoDto pagamento) {
+        String mensagem = """
+                Dados do pagamento: %s
+                Número do pedido: %s
+                Valor R$: %s
+                Status: %s 
+                """.formatted(pagamento.getId(),
+                pagamento.getPedidoId(),
+                pagamento.getValor(),
+                pagamento.getStatus());
+
+        System.out.println("Recebi a mensagem " + mensagem);
+    }
+}
